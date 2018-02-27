a66415635cb221cd832139d34b050d628f12100b
Enrique Flores <quiqueflores1307@gmail.com>
2018-02-27 00:02:48 -0600

    Agregando carpeta Prac2

---
+++ practicas/prac2.Makefiles/EnriqueFlores/muestreo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "defs.h"
+#include "procesamiento.h"
+#include "archivos.h"
+
+int main(){
+	float seno[MUESTRAS];
+
+	genera_seno (seno);
+	guarda_datos (seno);
+
+	return 0;
+}
